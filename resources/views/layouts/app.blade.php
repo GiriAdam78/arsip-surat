6535d1d7848679482bd634556c7a12f5202d0070
GiriAdam78 <giridiwa6@gmail.com>
2025-01-24 22:33:37 +0700

    Perubahan CSS Mixed content

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,13 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login Arsip Surat</title>
     {{-- CSS --}}
-    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    {{-- <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}"> --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
 <body>
     <div class="container">
         @yield('content')
     </div>
 
-    <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+    {{-- <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script> --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
